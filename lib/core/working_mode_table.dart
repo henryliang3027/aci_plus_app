c31663942163464743459df5c92caf44b1cae0e2
Henry.Liang <henryliang3027@gmail.com>
2023-12-19 08:58:04 +0800

    feat: :recycle: update working mode text

--- lib/core/working_mode_table.dart
+++ lib/core/working_mode_table.dart
@@ -3,9 +3,9 @@ class WorkingModeTable {
     '12': 'Auto Pilot (1ch & Last ch), AGC ON (Unlocked)',
     '13': 'Auto Pilot (1ch & Last ch), AGC ON (Locked)',
     '14': 'Auto Pilot (1ch & Last ch), Loss Pilot(In TGC)',
-    '22': 'Manual Pilot , In AGC (Unlocked)',
-    '23': 'Manual Pilot , In AGC (In Lock)',
-    '24': 'Manual Pilot , Loss Pilot(In TGC)',
+    '22': 'Manual Pilot, In AGC (Unlocked)',
+    '23': 'Manual Pilot, In AGC (In Lock)',
+    '24': 'Manual Pilot, Loss Pilot(In TGC)',
     '71': 'TGC + Log Input PAD',
     '72': 'TGC + Manual Input PAD',
   };
