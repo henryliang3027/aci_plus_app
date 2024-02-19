7b153d35e2fe1b6d9b52fcda3d0ea89aae17a936
Henry.Liang <henryliang3027@gmail.com>
2024-02-19 18:02:25 +0800

    feat: :construction: adding instruction page

---
+++ lib/advanced/view/setting18_instruction_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class Setting18InstructionPage extends StatelessWidget {
+  const Setting18InstructionPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Theme.of(context).colorScheme.onPrimary,
+      ),
+      child: const ContentView(),
+    );
+  }
+}
+
+class ContentView extends StatelessWidget {
+  const ContentView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [],
+    );
+  }
+}
