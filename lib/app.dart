e3f9a0803135b66e337eb762322fb4bdf4de8052
Henry.Liang <henryliang3027@gmail.com>
2024-03-20 15:26:26 +0800

    refactor: :recycle: remove splash view

--- lib/app.dart
+++ lib/app.dart
@@ -7,7 +7,6 @@ import 'package:aci_plus_app/repositories/amp18_ccor_node_repository.dart';
 import 'package:aci_plus_app/repositories/amp18_repository.dart';
 import 'package:aci_plus_app/repositories/gps_repository.dart';
 import 'package:aci_plus_app/repositories/unit_repository.dart';
-import 'package:aci_plus_app/splash_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
@@ -120,8 +119,6 @@ class _AppView extends StatelessWidget {
           } else {
             return const HomePage();
           }
-
-          // state.showSplash ? const SplashScreen() : const HomePage();
         },
       ),
     );
