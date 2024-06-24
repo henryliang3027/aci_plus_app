97a60193394627c62947875ed26e550fd14a4614
Henry.Liang <henryliang3027@gmail.com>
2024-06-24 09:21:19 +0800

    refactor: :recycle: update firmware file table

--- lib/core/firmware_file_table.dart
+++ lib/core/firmware_file_table.dart
@@ -4,7 +4,7 @@ class FirmwareFileTable {
     '2': 'assets/firmwares/BLE_R140.bin', // MOTO BLE
     '3': 'assets/firmwares/MB_R140.bin', // MOTO MB
     '4': 'assets/firmwares/OM_R135.bin', // C-Cor Node
-    '5': 'assets/firmwares/SDLE_R140.bin', // C-Cor TR
+    '5': 'assets/firmwares/TR_R139.bin', // C-Cor TR
     '6': 'assets/firmwares/BR_R139.bin', // C-Cor BR
     '7': 'assets/firmwares/LE_R139.bin', // C-Cor LE
     '8': 'assets/firmwares/SDAT_R140.bin', // SDAT
