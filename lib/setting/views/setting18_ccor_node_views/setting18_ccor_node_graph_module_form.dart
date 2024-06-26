f15fd4cbdcaeb200f55f5de93dffb74b60cf25bd
Henry.Liang <henryliang3027@gmail.com>
2024-06-26 18:07:34 +0800

    refactor:

--- lib/setting/views/setting18_ccor_node_views/setting18_ccor_node_graph_module_form.dart
+++ lib/setting/views/setting18_ccor_node_views/setting18_ccor_node_graph_module_form.dart
@@ -27,23 +27,9 @@ class Setting18CCorNodeGraphModuleForm extends StatefulWidget {
 
 class _Setting18CCorNodeGraphModuleFormState
     extends State<Setting18CCorNodeGraphModuleForm> {
-  late final Color _backgroundColor;
-  late final List<Widget> forwardSettingWidgets;
-  late final List<Widget> reverseSettingWidgets;
-
   @override
   void initState() {
     super.initState();
-
-    forwardSettingWidgets =
-        getForwardSettingWidgetByModuleId(widget.moduleName);
-    reverseSettingWidgets =
-        getReverseSettingWidgetByModuleId(widget.moduleName);
-
-    //  如果是下行的模組, 背景顏色顯示為淺藍色, 上行模組則為粉紅色
-    _backgroundColor = forwardSettingWidgets.isNotEmpty
-        ? CustomStyle.customBlue
-        : CustomStyle.customPink;
   }
 
   @override
@@ -142,8 +128,48 @@ class _Setting18CCorNodeGraphModuleFormState
       );
     }
 
+    Map<String, List<Widget>> forwardSettingWidgetsMap = {
+      DataKey.forwardConfig.name: [const _ForwardConfig()],
+      DataKey.splitOption.name: [const _SplitOption()],
+      DataKey.dsVVA1.name: [const _ForwardInputAttenuation1()],
+      DataKey.dsVVA3.name: [const _ForwardInputAttenuation3()],
+      DataKey.dsVVA4.name: [const _ForwardInputAttenuation4()],
+      DataKey.dsVVA6.name: [const _ForwardInputAttenuation6()],
+      DataKey.dsOutSlope1.name: [const _ForwardOutputEqualizer1()],
+      DataKey.dsOutSlope3.name: [const _ForwardOutputEqualizer3()],
+      DataKey.dsOutSlope4.name: [const _ForwardOutputEqualizer4()],
+      DataKey.dsOutSlope6.name: [const _ForwardOutputEqualizer6()],
+      DataKey.biasCurrent1.name: [const _ForwardBiasCurrent1()],
+      DataKey.biasCurrent3.name: [const _ForwardBiasCurrent3()],
+      DataKey.biasCurrent4.name: [const _ForwardBiasCurrent4()],
+      DataKey.biasCurrent6.name: [const _ForwardBiasCurrent6()],
+    };
+
+    Map<String, List<Widget>> reverseSettingWidgetsMap = {
+      DataKey.usVCA1.name: [
+        const _ReturnInputAttenuation1(),
+        const _ReturnIngressSetting1(),
+      ],
+      DataKey.usVCA3.name: [
+        const _ReturnInputAttenuation3(),
+        const _ReturnIngressSetting3(),
+      ],
+      DataKey.usVCA4.name: [
+        const _ReturnInputAttenuation4(),
+        const _ReturnIngressSetting4(),
+      ],
+      DataKey.usVCA6.name: [
+        const _ReturnInputAttenuation6(),
+        const _ReturnIngressSetting6(),
+      ],
+    };
+
     List<Widget> getSettingWidgetByModuleId() {
       List<Widget> settingWidgets = [];
+      List<Widget> forwardSettingWidgets =
+          forwardSettingWidgetsMap[widget.moduleName] ?? [];
+      List<Widget> reverseSettingWidgets =
+          reverseSettingWidgetsMap[widget.moduleName] ?? [];
 
       Widget forwardSettingHeader = getSettingWidgetHeader(
           AppLocalizations.of(context)!.forwardControlParameters);
@@ -161,6 +187,14 @@ class _Setting18CCorNodeGraphModuleFormState
       }
 
       return settingWidgets;
+    }
+
+    Color getBackgroundColor() {
+      bool isForwardWidget =
+          forwardSettingWidgetsMap.keys.contains(widget.moduleName);
+
+      // 如果是下行模組, 就將背景設為淺藍色, 否則就視為上行模組, 設為粉紅色
+      return isForwardWidget ? CustomStyle.customBlue : CustomStyle.customPink;
     }
 
     Color getResultValueColor(String resultValue) {
@@ -239,7 +273,9 @@ class _Setting18CCorNodeGraphModuleFormState
         }
       },
       child: Container(
-        decoration: BoxDecoration(color: _backgroundColor),
+        decoration: BoxDecoration(
+          color: getBackgroundColor(),
+        ),
         child: Padding(
           padding: const EdgeInsets.all(30),
           child: Stack(
@@ -265,92 +301,6 @@ class _Setting18CCorNodeGraphModuleFormState
         ),
       ),
     );
-  }
-}
-
-List<Widget> getForwardSettingWidgetByModuleId(String moduleName) {
-  if (moduleName == DataKey.forwardConfig.name) {
-    return [
-      const _ForwardConfig(),
-    ];
-  } else if (moduleName == DataKey.splitOption.name) {
-    return [const _SplitOption()];
-  } else if (moduleName == DataKey.dsVVA1.name) {
-    return [
-      const _ForwardInputAttenuation1(),
-    ];
-  } else if (moduleName == DataKey.dsVVA3.name) {
-    return [
-      const _ForwardInputAttenuation3(),
-    ];
-  } else if (moduleName == DataKey.dsVVA4.name) {
-    return [
-      const _ForwardInputAttenuation4(),
-    ];
-  } else if (moduleName == DataKey.dsVVA6.name) {
-    return [
-      const _ForwardInputAttenuation6(),
-    ];
-  } else if (moduleName == DataKey.dsOutSlope1.name) {
-    return [
-      const _ForwardOutputEqualizer1(),
-    ];
-  } else if (moduleName == DataKey.dsOutSlope3.name) {
-    return [
-      const _ForwardOutputEqualizer3(),
-    ];
-  } else if (moduleName == DataKey.dsOutSlope4.name) {
-    return [
-      const _ForwardOutputEqualizer4(),
-    ];
-  } else if (moduleName == DataKey.dsOutSlope6.name) {
-    return [
-      const _ForwardOutputEqualizer6(),
-    ];
-  } else if (moduleName == DataKey.biasCurrent1.name) {
-    return [
-      const _ForwardBiasCurrent1(),
-    ];
-  } else if (moduleName == DataKey.biasCurrent3.name) {
-    return [
-      const _ForwardBiasCurrent3(),
-    ];
-  } else if (moduleName == DataKey.biasCurrent4.name) {
-    return [
-      const _ForwardBiasCurrent4(),
-    ];
-  } else if (moduleName == DataKey.biasCurrent6.name) {
-    return [
-      const _ForwardBiasCurrent6(),
-    ];
-  } else {
-    return [];
-  }
-}
-
-List<Widget> getReverseSettingWidgetByModuleId(String moduleName) {
-  if (moduleName == DataKey.usVCA1.name) {
-    return [
-      const _ReturnInputAttenuation1(),
-      const _ReturnIngressSetting1(),
-    ];
-  } else if (moduleName == DataKey.usVCA3.name) {
-    return [
-      const _ReturnInputAttenuation3(),
-      const _ReturnIngressSetting3(),
-    ];
-  } else if (moduleName == DataKey.usVCA4.name) {
-    return [
-      const _ReturnInputAttenuation4(),
-      const _ReturnIngressSetting4(),
-    ];
-  } else if (moduleName == DataKey.usVCA6.name) {
-    return [
-      const _ReturnInputAttenuation6(),
-      const _ReturnIngressSetting6(),
-    ];
-  } else {
-    return [];
   }
 }
 
