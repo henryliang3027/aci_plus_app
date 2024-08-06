7898cbaa14ad1cd3670881738b4d84fcad956115
Henry.Liang <henryliang3027@gmail.com>
2024-08-06 17:59:47 +0800

    refactor: :recycle: enable warm reset and theme

--- lib/information/views/information18_form.dart
+++ lib/information/views/information18_form.dart
@@ -108,50 +108,50 @@ class __PopupMenuState extends State<_PopupMenu> {
                     context.read<HomeBloc>().add(const DeviceRefreshed());
                   },
                 ),
-                // menuItem(
-                //   value: HomeMenu.theme,
-                //   iconData: Icons.colorize_rounded,
-                //   title: AppLocalizations.of(context)!.theme,
-                //   onTap: () {
-                //     showThemeOptionDialog(context: context).then(
-                //       (String? theme) {
-                //         if (theme != null) {
-                //           changeThemeByThemeString(
-                //             context: context,
-                //             theme: theme,
-                //           );
-                //         }
-                //       },
-                //     );
-                //   },
-                // ),
-                // menuItem(
-                //   value: HomeMenu.warmReset,
-                //   iconData: Icons.restart_alt_outlined,
-                //   title: AppLocalizations.of(context)!.warmReset,
-                //   onTap: () {
-                //     context
-                //         .read<Information18Bloc>()
-                //         .add(const AlarmPeriodicUpdateCanceled());
+                menuItem(
+                  value: HomeMenu.theme,
+                  iconData: Icons.colorize_rounded,
+                  title: AppLocalizations.of(context)!.theme,
+                  onTap: () {
+                    showThemeOptionDialog(context: context).then(
+                      (String? theme) {
+                        if (theme != null) {
+                          changeThemeByThemeString(
+                            context: context,
+                            theme: theme,
+                          );
+                        }
+                      },
+                    );
+                  },
+                ),
+                menuItem(
+                  value: HomeMenu.warmReset,
+                  iconData: Icons.restart_alt_outlined,
+                  title: AppLocalizations.of(context)!.warmReset,
+                  onTap: () {
+                    context
+                        .read<Information18Bloc>()
+                        .add(const AlarmPeriodicUpdateCanceled());
 
-                //     showWarmResetNoticeDialog(context: context).then(
-                //       (isConfirm) {
-                //         if (isConfirm != null) {
-                //           if (isConfirm) {
-                //             showWarmResetDialog(context: context).then((_) {
-                //               showWarmResetSuccessDialog(context: context)
-                //                   .then((_) {
-                //                 context
-                //                     .read<HomeBloc>()
-                //                     .add(const Data18Requested());
-                //               });
-                //             });
-                //           }
-                //         }
-                //       },
-                //     );
-                //   },
-                // ),
+                    showWarmResetNoticeDialog(context: context).then(
+                      (isConfirm) {
+                        if (isConfirm != null) {
+                          if (isConfirm) {
+                            showWarmResetDialog(context: context).then((_) {
+                              showWarmResetSuccessDialog(context: context)
+                                  .then((_) {
+                                context
+                                    .read<HomeBloc>()
+                                    .add(const Data18Requested());
+                              });
+                            });
+                          }
+                        }
+                      },
+                    );
+                  },
+                ),
                 menuItem(
                   value: HomeMenu.about,
                   iconData: CustomIcons.about,
