7e010ce3f1fba8779b59102fb830b744ff4d8c20
Henry.Liang <henryliang3027@gmail.com>
2024-08-23 14:12:08 +0800

    refactor: :recycle: remove old circuit svg

--- lib/core/utils.dart
+++ lib/core/utils.dart
@@ -121,6 +121,6 @@ Future<String> getAppVersion() async {
   PackageInfo packageInfo = await PackageInfo.fromPlatform();
 
   // 給部門內測試的版本會加 -beta版本文字, 例如V 2.1.2-beta2
-  String appVersion = 'V ${packageInfo.version}-beta3';
+  String appVersion = 'V ${packageInfo.version}-beta8';
   return appVersion;
 }
